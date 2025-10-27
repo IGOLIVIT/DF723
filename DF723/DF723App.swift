20f820ca1b9ac1e090ff0f0a5293852a3e0341a3
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2025-10-27 13:23:33 +0300

    new3

--- DF723/DF723App.swift
+++ DF723/DF723App.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 @main
 struct DF723App: App {
@@ -22,7 +23,7 @@ struct DF723App: App {
                 
                 if isFetched == false {
                     
-                    Text("")
+                    ProgressView()
                     
                 } else if isFetched == true {
                     
@@ -42,33 +43,11 @@ struct DF723App: App {
             }
             .onAppear {
                 
-                check_data()
+                makeServerRequest()
             }
         }
     }
-    
-    private func check_data() {
-        
-        let lastDate = "29.10.2025"
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.yyyy"
-        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
-        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
-        let now = Date()
-        
-        guard now > targetDate else {
-            
-            isBlock = true
-            isFetched = true
-            
-            return
-        }
-        
-        // Дата в прошлом - делаем запрос на сервер
-        makeServerRequest()
-    }
-    
+
     private func makeServerRequest() {
         
         let dataManager = DataManagers()
@@ -109,108 +88,10 @@ struct DF723App: App {
             
         }.resume()
     }
+    
+
 }
 
 
-//struct ContentView: View {
-//    
-//    @State var isFetched: Bool = false
-//    
-//    @AppStorage("isBlock") var isBlock: Bool = true
-//    @AppStorage("isRequested") var isRequested: Bool = false
-//    
-//    var body: some View {
-//        
-//        ZStack {
-//            
-//            if isFetched == false {
-//                
-//                Text("")
-//                
-//            } else if isFetched == true {
-//                
-//                if isBlock == true {
-//                    
-//                    if dataManager.hasCompletedOnboarding {
-//                        MainTabView()
-//                    } else {
-//                        OnboardingView()
-//                    }
-//                    
-//                } else if isBlock == false {
-//                    
-//                    WebSystem()
-//                }
-//            }
-//        }
-//        .onAppear {
-//            
-//            check_data()
-//        }
-//    }
-//    
-//    
-//    
-//    private func check_data() {
-//        
-//        let lastDate = "12.10.2025"
-//        
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "dd.MM.yyyy"
-//        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
-//        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
-//        let now = Date()
-//        
-//        guard now > targetDate else {
-//            
-//            isBlock = true
-//            isFetched = true
-//            
-//            return
-//        }
-//        
-//        // Дата в прошлом - делаем запрос на сервер
-//        makeServerRequest()
-//    }
-//    
-//    private func makeServerRequest() {
-//        
-//        let dataManager = DataManagers()
-//        
-//        guard let url = URL(string: dataManager.server) else {
-//            self.isBlock = true
-//            self.isFetched = true
-//            return
-//        }
-//        
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//        
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            
-//            DispatchQueue.main.async {
-//                
-//                if let httpResponse = response as? HTTPURLResponse {
-//                    
-//                    if httpResponse.statusCode == 404 {
-//                        
-//                        self.isBlock = true
-//                        self.isFetched = true
-//                        
-//                    } else if httpResponse.statusCode == 200 {
-//                        
-//                        self.isBlock = false
-//                        self.isFetched = true
-//                    }
-//                    
-//                } else {
-//                    
-//                    // В случае ошибки сети тоже блокируем
-//                    self.isBlock = true
-//                    self.isFetched = true
-//                }
-//            }
-//            
-//        }.resume()
-//    }
-//}
+
+
